733784f13e514323d51bc89afe05f866a571fc89
Xenon Chau <cto@ido-game.com>
2017-06-15 16:33:10 +0800

    修改slider文件名

--- index.android.js
+++ index.android.js
@@ -1,33 +1,92 @@
-import React, {Component} from 'react';
-import {
-    View,
+/***** Slider  http://blog.csdn.net/p106786860/article/details/51338764
+ */
+
+'use strict';
+var React = require('react');
+var ReactNative = require('react-native');
+
+var {
     AppRegistry,
-    WebView,
-    NativeModules,
-} from 'react-native';
-import VideoView from './VideoView';
+    StyleSheet,
+    View,
+    Text,
+    Slider,
+} = ReactNative;
 
-export default class VideoPlayScene extends Component {
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
+var SliderExample = React.createClass({
+    getDefaultProps: function(){
+        return {
+            value: 0,
+        };
+    },
+    getInitialState: function(){
+        return {
+            value: 0,
+        };
+    },
+    render: function(){
         return (
-            <View style={{flex:1,alignItems:'center',justifyContent:'center',}}>
-                <VideoView
-                    style={{height:250,width:380}}
-                    source={
-                        {
-                            url:'http://qiubai-video.qiushibaike.com/A14EXG7JQ53PYURP.mp4',
-                            headers:{
-                                'refer':'myRefer'
-                            }
-                        }
-                    }
-                />
+            <View>
+                <Text style={styles.text}>
+                    {this.state.value && +this.state.value.toFixed(3)}
+                </Text>
+                <Slider
+                    {...this.props}
+                    onValueChange={(value) => this.setState({value: value})} />
+            </View>
+        );
+    },
+});
+
+var SlidingCompleteExample = React.createClass({
+    getInitialState: function(){
+        return {
+            slideCompletionValue: 0,
+            slideCompletionCount: 0,
+        };
+    },
+
+    render: function() {
+        return (
+            <View>
+                <SliderExample
+                    {...this.props}
+                    onSlidingComplete={(value) => this.setState({
+                        slideCompletionValue: value,
+                        slideCompletionCount: this.state.slideCompletionCount + 1})} />
+                <Text>
+                    Completions: {this.state.slideCompletionCount} Value: {this.state.slideCompletionValue}
+                </Text>
             </View>
         );
     }
-}
-AppRegistry.registerComponent('SimpleVideoView', () => VideoPlayScene);
+});
+
+var helloworld = React.createClass({
+    render: function(){
+        return(
+            <View>
+                <SliderExample />
+                <SliderExample value={0.5} />
+                <SliderExample minimumValue={-1} maximumValue={2}/>
+                <SliderExample step={0.25} />
+                <SlidingCompleteExample />
+            </View>
+        );
+    }
+});
+
+var styles = StyleSheet.create({
+    slider:{
+        height: 10,
+        width: 10,
+    },
+    text:{
+        fontSize: 14,
+        textAlign: 'center',
+        fontWeight: '500',
+        margin: 10,
+    },
+});
+
+AppRegistry.registerComponent('helloworld', () => helloworld);
